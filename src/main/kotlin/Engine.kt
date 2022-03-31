e2b221236a98058779805290e7c71e1d0776c1d7
julianczaja <julianczaja97@gmail.com>
2022-03-31 19:25:13 +0200

    Refactored initWorld function

--- src/main/kotlin/Engine.kt
+++ src/main/kotlin/Engine.kt
@@ -99,10 +99,10 @@ class Engine(
 
             Debug.logd(TAG, "deltaTime: $deltaTime")
 
-            (coordinateSystemEntity.getComponent(TransformComponent::class.java.name) as TransformComponent).rotatable.rotateBy(
-                0.001f * deltaTime,
-                Vec3(0f, 1f, 0f)
-            )
+//            (coordinateSystemEntity.getComponent(TransformComponent::class.java.name) as TransformComponent).rotatable.rotateBy(
+//                0.001f * deltaTime,
+//                Vec3(0f, 1f, 0f)
+//            )
             ecs.update(deltaTime)
 
             // Read OpenGL error
@@ -161,13 +161,37 @@ class Engine(
     }
 
     private fun initWorld() {
-        val terrain = Terrain(10, 0.1f, 1f)
+        // Camera
+        ecs.addEntity(
+            Entity()
+                .addComponent(
+                    CameraComponent(window)
+                )
+                .addComponent(
+                    TransformComponent().apply {
+                        movable.moveTo(0f, 5f, 0f)
+                    }
+                )
+        )
 
+        // Terrain
+        val terrain = Terrain(50, 0.05f, 1f)
+        ecs.addEntity(
+            Entity()
+                .addComponent(
+                    TransformComponent()
+                )
+                .addComponent(
+                    ModelComponent(terrain)
+                )
+        )
+
+        // Tree
         ecs.addEntity(
             Entity()
                 .addComponent(
                     TransformComponent().apply {
-                        movable = Movable(Vec3(5f, terrain.getHeightAt(5f, -5f), -5f))
+                        movable = Movable(Vec3(5f, terrain.getHeightAt(5f, -10f), -10f))
                         rotatable = Rotatable(Quat(glm.radians(Vec3(0f))))
                     })
                 .addComponent(
@@ -178,9 +202,9 @@ class Engine(
                         )
                     )
                 )
-                .addComponent(
-                    VelocityComponent(Vec3(-0.0001f, 0f, -0.0001f)) // TODO: refactor to 1m/s
-                )
+//                .addComponent(
+//                    VelocityComponent(Vec3(-0.0001f, 0f, -0.0001f)) // TODO: refactor to 1m/s
+//                )
                 .addComponent(
                     CollisionComponent(
                         boundingBoxType = BoundingBoxType.AXIS_ALIGNED,
@@ -189,6 +213,34 @@ class Engine(
                 )
         )
 
+        // Rock
+        ecs.addEntity(
+            Entity()
+                .addComponent(
+                    TransformComponent().apply {
+                        movable = Movable(Vec3(10f, terrain.getHeightAt(10f, -10f), -10f))
+                        rotatable = Rotatable(Quat(glm.radians(Vec3(0f))))
+                    })
+                .addComponent(
+                    ModelComponent(
+                        ModelDefault(
+                            mesh = ModelLoader.loadStaticModel("src/main/resources/Models/rock1.obj"),
+                            texture = Texture.getDefaultColorPalette()
+                        )
+                    )
+                )
+//                .addComponent(
+//                    VelocityComponent(Vec3(-0.0001f, 0f, -0.0001f)) // TODO: refactor to 1m/s
+//                )
+                .addComponent(
+                    CollisionComponent(
+                        boundingBoxType = BoundingBoxType.AXIS_ALIGNED,
+                        optimize = false
+                    )
+                )
+        )
+
+        // Coordinate system
         coordinateSystemEntity = Entity()
             .addComponent(
                 TransformComponent()
@@ -209,27 +261,6 @@ class Engine(
             )
         ecs.addEntity(coordinateSystemEntity)
 
-        ecs.addEntity(
-            Entity()
-                .addComponent(
-                    TransformComponent()
-                )
-                .addComponent(
-                    ModelComponent(terrain)
-                )
-        )
-        // Camera
-        ecs.addEntity(
-            Entity()
-                .addComponent(
-                    CameraComponent(window)
-                )
-                .addComponent(
-                    TransformComponent().apply {
-                        movable.moveTo(0f, 5f, 0f)
-                    }
-                )
-        )
         // Lights
         ecs.addEntity(
             Entity()
@@ -238,7 +269,8 @@ class Engine(
                         LightAmbient().apply {
                             color = Vec3(1f, 1f, 1f)
                             intensity = 0.7f
-                        })
+                        }
+                    )
                 )
         )
         ecs.addEntity(
@@ -257,7 +289,7 @@ class Engine(
             Entity()
                 .addComponent(
                     LightComponent(
-                        LightPoint(0, Movable(Vec3(0f, 2f, 0f))).apply {
+                        LightPoint(0, Movable(Vec3(2f, 1f, -2f))).apply {
                             color = Vec3(0f, 0f, 1f)
                             intensity = 1f
                             kc = 1.0f
@@ -271,7 +303,7 @@ class Engine(
             Entity()
                 .addComponent(
                     LightComponent(
-                        LightSpot(0, Movable(Vec3(5f, 2f, 0f))).apply {
+                        LightSpot(0, Movable(Vec3(7f, 2f, -2f))).apply {
                             color = Vec3(0.8f, 0.65f, 0.4f)
                             intensity = 0.3f
                             direction = Vec3(0f, -1f, 0f)
@@ -279,117 +311,21 @@ class Engine(
                             innerAngle = glm.cos(glm.radians(20f))
                         }
                     )
-                ))
-
+                )
+        )
+        ecs.addEntity(
+            Entity()
+                .addComponent(
+                    LightComponent(
+                        LightSpot(1, Movable(Vec3(12f, 2f, -2f))).apply {
+                            color = Vec3(1.0f, 0.0f, 0.0f)
+                            intensity = 0.3f
+                            direction = Vec3(0f, -1f, 0f)
+                            outerAngle = glm.cos(glm.radians(50f))
+                            innerAngle = glm.cos(glm.radians(20f))
+                        }
+                    )
+                )
+        )
     }
-//        val colorPaletteTexture = Texture.getDefaultColorPalette()
-//
-//        // models.Base.Terrain
-//        val terrain = Terrain(10, 0.1f, 5.0f)
-//        world.addTerrain(terrain)
-//
-//        // Coordinate system origin helpful model
-//        val coordinateSystem = CoordinateSystem()
-//        world.addModelNoLight(coordinateSystem)
-//
-//        // Tree
-//        val tree1 = Tree(TreeType.TYPE_1)
-//        tree1.moveTo(10f, terrain.getHeightAt(10f, -10f), -10f)
-//        world.addModelDefault(tree1)
-//        val tree2 = Tree(TreeType.TYPE_2)
-//        tree2.moveTo(20f, terrain.getHeightAt(20f, -10f), -10f)
-//        world.addModelDefault(tree2)
-//        val tree3 = Tree(TreeType.TYPE_3)
-//        tree3.moveTo(30f, terrain.getHeightAt(30f, -10f), -10f)
-//        world.addModelDefault(tree3)
-//
-//        // Rock
-//        val rock1 = Rock(RockType.TYPE_1)
-//        rock1.moveTo(10f, terrain.getHeightAt(10f, -20f), -20f)
-//        world.addModelDefault(rock1)
-//        val rock2 = Rock(RockType.TYPE_2)
-//        rock2.moveTo(20f, terrain.getHeightAt(20f, -20f), -20f)
-//        world.addModelDefault(rock2)
-//        val rock3 = Rock(RockType.TYPE_3)
-//        rock3.moveTo(30f, terrain.getHeightAt(30f, -20f), -20f)
-//        world.addModelDefault(rock3)
-//
-//        // Pig model from: http://quaternius.com/
-//        val r = Random(1234567)
-//        for (i in 1..5) {
-//            val pig = Pig(PigType.TYPE_1)
-//            val scale = r.nextInt(8, 10) / 10f
-//            val randX = r.nextFloat() * terrain.size * terrain.tileSize
-//            val randZ = -r.nextFloat() * terrain.size * terrain.tileSize
-//            val height = terrain.getHeightAt(randX, randZ)
-//            pig.rotateTo(r.nextFloat() * 360f, 0f, 0f)
-//            pig.scaleTo(scale)
-//            pig.moveTo(randX, height, randZ)
-//            world.addModelDefault(pig)
-//        }
-//
-//        // Light source model
-//        val lampModel1 = SphereLamp()
-//        lampModel1.addTexture(colorPaletteTexture)
-//        lampModel1.scaleTo(0.3f, 0.3f, 0.3f)
-//        lampModel1.moveTo(2f, terrain.getHeightAt(2f, -2f) + 3f, -2f)
-//        world.addModelNoLight(lampModel1)
-//
-//        val lampModel2 = SphereLamp()
-//        lampModel2.addTexture(colorPaletteTexture)
-//        lampModel2.scaleTo(0.3f, 0.3f, 0.3f)
-//        lampModel2.moveTo(19f, terrain.getHeightAt(19f, -19f) + 3f, -19f)
-//        world.addModelNoLight(lampModel2)
-//
-//        val streetLamp = StreetLamp()
-//        streetLamp.moveTo(5f, terrain.getHeightAt(5f, -5f), -5f)
-//        world.addModelDefault(streetLamp)
-//
-//        // Lighting
-//        val ambientLight = LightAmbient()
-//        ambientLight.color = Vec3(1f, 1f, 1f)
-//        ambientLight.intensity = 0.7f
-//        world.addLightSource(ambientLight)
-//
-//        val directionalLight = LightDirectional()
-//        directionalLight.color = Vec3(1f, 1f, 1f)
-//        directionalLight.intensity = 0.2f
-//        directionalLight.direction = Vec3(-1.0f, -0.5f, -0.5f)
-//        world.addLightSource(directionalLight)
-//
-//        val pointLight1 = LightPoint(0, lampModel1.position)
-//        pointLight1.color = Vec3(0f, 0f, 1f)
-//        pointLight1.intensity = 1f
-//        pointLight1.kc = 1.0f
-//        pointLight1.kl = 0.14f
-//        pointLight1.kq = 0.07f
-//        world.addLightSource(pointLight1)
-//
-//        val pointLight2 = LightPoint(1, lampModel2.position)
-//        pointLight2.color = Vec3(1f, 0.0f, 0.0f)
-//        pointLight2.intensity = 1f
-//        pointLight2.kc = 1.0f
-//        pointLight2.kl = 0.14f
-//        pointLight2.kq = 0.07f
-//        world.addLightSource(pointLight2)
-//
-//        val spotLight = LightSpot(
-//            index = 0,
-//            position = Vec3(
-//                streetLamp.position.x,
-//                streetLamp.position.y + 2.75f,
-//                streetLamp.position.z + 1.4f
-//            )
-//        )
-//        spotLight.color = Vec3(0.8f, 0.65f, 0.4f)
-//        spotLight.intensity = 0.3f
-//        spotLight.direction = Vec3(0f, -1f, 0f)
-//        spotLight.outerAngle = glm.cos(glm.radians(50f))
-//        spotLight.innerAngle = glm.cos(glm.radians(20f))
-//        world.addLightSource(spotLight)
-//
-//        // Skybox
-//        val skybox = Skybox()
-//        world.skybox = skybox
-//    }
 }
